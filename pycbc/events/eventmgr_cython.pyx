04319e6e85c7e0d36177cba75cb3b6e82c240d12
Ian Harry <ian.harry@ligo.org>
2022-09-15 02:19:12 -0700

    Need to import round

--- pycbc/events/eventmgr_cython.pyx
+++ pycbc/events/eventmgr_cython.pyx
@@ -2,6 +2,7 @@ import numpy as np
 cimport numpy as cnp
 from cython import wraparound, boundscheck, cdivision
 from libc.math cimport M_PI, sqrt
+from libc.math cimport round as cround
 
 
 ctypedef fused REALTYPE:
